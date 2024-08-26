60e30cc0ae164d0b37020a268259e2318ce9c5bd
joniox <jonksanen@gmail.com>
2024-08-26 12:53:52 +0300

    Moduuli 2, Tehtävä 4

---
+++ Moduuli 2, Tehtävä 4.py
@@ -0,0 +1,7 @@
+ensimmäinen = float(input("Anna ensimmäinen arvo "))
+toinen = float(input("Anna toinen arvo "))
+kolmas = float(input("Anna kolmas arvo "))
+summa = ensimmäinen + toinen + kolmas
+tulo = ensimmäinen * toinen * kolmas
+keskiarvo = (ensimmäinen + toinen + kolmas) / 3
+print(f"Arvojen summa on {summa}, tulo on {tulo} ja keskiarvo on {keskiarvo}")
